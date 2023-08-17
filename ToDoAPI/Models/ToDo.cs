a820353f051eb61a09c5e9c719aaac94dbec29c2
Sam <ullrichsam@gmail.com>
2023-08-17 15:28:54 -0700

    Model and Data Framework setup

---
+++ ToDoAPI/Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAPI.Models
+{
+    public class ToDo
+    {
+        //The Key attribute is used to denote the property that uniquely identifies
+        //an entity (the EntityKey ), and which is mapped to the Primary Key field
+        //in a database: 
+        [Key]
+        public int Id { get; set; }
+
+        public string ToDoName { get; set; }
+    }
+}
