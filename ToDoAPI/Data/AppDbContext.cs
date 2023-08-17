f06bf509a1c48a33dad22194160933349a0f3475
Sam <ullrichsam@gmail.com>
2023-08-17 15:39:07 -0700

    Registered database context for registry injection connection string

--- ToDoAPI/Data/AppDbContext.cs
+++ ToDoAPI/Data/AppDbContext.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Channels;
 using ToDoAPI.Models;
-
 namespace ToDoAPI.Data 
 {
+/*  A DbContext instance represents a combination of the Unit Of Work 
+    and Repository patterns such that it can be used to query from 
+    a database and group together changes that will then be written 
+    back to the store as a unit.DbContext is conceptually similar 
+    to ObjectContext.*/
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
