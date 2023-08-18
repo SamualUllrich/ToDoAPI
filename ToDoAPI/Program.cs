77eeea79f9782696ef587027e228e2a1eba25006
Sam <ullrichsam@gmail.com>
2023-08-18 14:04:05 -0700

    Set API endpoints (Tested with Insomnia)

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using ToDoAPI.Data;
+using ToDoAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +16,21 @@ var app = builder.Build();
 
 //app.UseHttpsRedirection();
 
+app.MapGet("api/todo", async (AppDbContext context) =>
+    {
+        var items = await context.ToDos.ToListAsync();
 
+        return Results.Ok(items);
+    });
+
+app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
+{
+    await context.ToDos.AddAsync(toDo);
+
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/todo/{toDo.Id}", toDo);
+});
 
 app.Run();
 
